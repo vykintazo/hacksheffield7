e35e90799ba4f67766cf70fe52e88d73708105da
barrowben <babarrow1@sheffield.ac.uk>
2022-11-05 16:00:54 +0000

    menu bar for customer page

--- src/customer/CustomerPage.jsx
+++ src/customer/CustomerPage.jsx
@@ -1,16 +1,22 @@
 import Map from "react-map-gl";
+import MenuBar from "./MenuBar";
 
 export default function CustomerPage() {
     return (
-        <Map
-            initialViewState={{
-                longitude: -1.477,
-                latitude: 53.378,
-                zoom: 13.21
-            }}
-            style={{width: '100vw', height: '100vh'}}
-            mapStyle="mapbox://styles/illuminatiboat/cla42292y00ns14p07a4ipgzp"
-            mapboxAccessToken=import.meta.env.APP_MAPBOX_TOKEN
-        />
+        <div>
+            <Map
+                initialViewState={{
+                    longitude: -1.477,
+                    latitude: 53.378,
+                    zoom: 13.21
+                }}
+                style={{width: '100vw', height: '100vh'}}
+                mapStyle="mapbox://styles/illuminatiboat/cla42292y00ns14p07a4ipgzp"
+                mapboxAccessToken=import.meta.env.APP_MAPBOX_TOKEN
+            >
+            <MenuBar/>
+            </Map>
+
+        </div>
     );
 }
---
+++ src/customer/MenuBar.jsx
@@ -0,0 +1,95 @@
+import {React, useState} from 'react';
+import { Global } from '@emotion/react';
+import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+import Skeleton from '@mui/material/Skeleton';
+import Typography from '@mui/material/Typography';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+
+const drawerBleeding = 56;
+
+const Root = styled('div')(({ theme }) => ({
+  height: '100%',
+  backgroundColor:
+    theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}));
+
+export default function SwipeableEdgeDrawer() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+
+  return (
+    <div>
+      <Global
+        styles={{
+          '.MuiDrawer-root > .MuiPaper-root': {
+            height: `calc(50% - ${drawerBleeding}px)`,
+            overflow: 'visible',
+          },
+        }}
+      />
+
+      <SwipeableDrawer
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+            zIndex: 5000
+        }}
+      >
+        <StyledBox
+          sx={{
+            background: 'white',
+            position: 'absolute',
+            top: -drawerBleeding,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            visibility: 'visible',
+            right: 0,
+            left: 0
+          }}
+        >
+          <Puller 
+            onClick= {() => toggleDrawer(true) }
+          />
+          <Typography sx={{ p: 2, color: 'text.secondary' }}>Nearby Offers</Typography>
+        </StyledBox>
+        <StyledBox
+          sx={{
+            px: 2,
+            pb: 2,
+            height: '100%',
+            overflow: 'auto',
+          }}
+        >
+          <Skeleton variant="rectangular" height="100%" />
+        </StyledBox>
+      </SwipeableDrawer>
+      </div>
+  );
+}
