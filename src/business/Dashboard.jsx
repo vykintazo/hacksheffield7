d92bfa4223e31984c0e97c5e4ed6d5d9c742c408
barrowben <babarrow1@sheffield.ac.uk>
2022-11-05 19:56:24 +0000

    updated dashboard to access offers from db

--- src/business/Dashboard.jsx
+++ src/business/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,66 +7,60 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-
-// Just add some dummy data
-function createData(
-  businessName,
-  offerName,
-  offerType,
-  startTime,
-  endTime,
-  amount,
-  numAvailable,
-  description
-) { return {businessName, offerName, offerType, startTime, endTime, amount, numAvailable, description}; }
-
-const rows = [
-  createData('businessName1', '2 for 1', 'BOGOF', "05.11.2022 08:00", "05.11.2022 23:59", 7, 100, "Buy one burrito, get one free. His and hers, matching burritos"),
-  createData('businessName2', '3 for 1', 'Free item', "05.11.2022 08:00", "05.11.2022 23:59", 7, 100, "Buy one burrito, get one free. His and hers, matching burritos"),
-  createData('businessName3', '4 for 1', 'Bulk buy', "05.11.2022 08:00", "05.11.2022 23:59", 7, 100, "Buy one burrito, get one free. His and hers, matching burritos"),
-  createData('businessName4', '5 for 1', 'Price slash', "05.11.2022 08:00", "05.11.2022 23:59", 7, 100, "Buy one burrito, get one free. His and hers, matching burritos"),
-  createData('businessName5', '6 for 1', 'Exclusive item', "05.11.2022 08:00", "05.11.2022 23:59", 7, 100, "Buy one burrito, get one free. His and hers, matching burritos"),
-];
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { collection, query } from "firebase/firestore"; 
+import { CircularProgress } from '@mui/material';
 
 export default function BasicTable() {
-  return (
-    <>
-        <Typography variant="h1" sx={{textAlign: 'center'}}>Company Name</Typography>;
 
-        <TableContainer component={Paper}>
-            <Table sx={{
-                    width: '80vw',
-                    marginLeft: 'auto',
-                    marginRight: 'auto' }}
-                    aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>businessName</TableCell>
-                    <TableCell align="right">offerName</TableCell>
-                    <TableCell align="right">offerType</TableCell>
-                    <TableCell align="right">startTime</TableCell>
-                    <TableCell align="right">endTime</TableCell>
-                    <TableCell align="right">amount</TableCell>
-                    <TableCell align="right">numAvailable</TableCell>
-                    <TableCell align="right">description</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
+    const firestore = useFirestore();
+    const offerCollection = collection(firestore, 'offers');
+    const offersQuery = query(offerCollection);
+
+    const { status, data } = useFirestoreCollectionData(offersQuery, {
+        idField: 'id', // this field will be added to the object created from each document
+      });
+
+      return (
+        <>
+            <Typography variant="h1" sx={{textAlign: 'center'}}>Company Name</Typography>;
+            { status === 'loading' ? <CircularProgress/> : 
+
+            <TableContainer component={Paper}>
+                <Table sx={{
+                        width: '80vw',
+                        marginLeft: 'auto',
+                        marginRight: 'auto' }}
+                        aria-label="simple table">
+                    <TableHead>
                     <TableRow>
-                        <TableCell align="right">{row.businessName}</TableCell>
-                        <TableCell align="right">{row.offerName}</TableCell>
-                        <TableCell align="right">{row.offerType}</TableCell>
-                        <TableCell align="right">{row.startTime}</TableCell>
-                        <TableCell align="right">{row.endTime}</TableCell>
-                        <TableCell align="right">{row.amount}</TableCell>
-                        <TableCell align="right">{row.numAvailable}</TableCell>
-                        <TableCell align="right">{row.description}</TableCell>
+                        <TableCell align="right">offerName</TableCell>
+                        <TableCell align="right">startTime</TableCell>
+                        <TableCell align="right">endTime</TableCell>
+                        <TableCell align="right">discountAmount</TableCell>
+                        <TableCell align="right">numAvailable</TableCell>
+                        <TableCell align="right">description</TableCell>
                     </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </>
-  );
+                    </TableHead>
+                    <TableBody>
+                    {status ==='loading' ? <CircularProgress /> : data.map((offer, i) => (
+                        <TableRow
+                        key={i} >
+                            <TableCell align="right">{offer.offerName}</TableCell>
+
+                            <TableCell align="right">{(Date(offer.startTime?.seconds))}</TableCell>
+                            <TableCell align="right">{(Date(offer.endTime?.seconds))}</TableCell>
+  
+                            <TableCell align="right">{offer.discountAmount}</TableCell>
+                            <TableCell align="right">{offer.numAvailable}</TableCell>
+                            <TableCell align="right">{offer.description}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>}
+
+        </>
+    
+      );
 }
