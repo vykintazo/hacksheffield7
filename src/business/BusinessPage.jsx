c78a01ee7a254986a4d4440dc2188fa59d4f1d95
barrowben <babarrow1@sheffield.ac.uk>
2022-11-05 17:47:10 +0000

    btn for add new discount

--- src/business/BusinessPage.jsx
+++ src/business/BusinessPage.jsx
@@ -1,8 +1,19 @@
 import Dashboard from "./Dashboard";
+import Box from '@mui/material/Box';
+import { Button } from "@mui/material";
 
 export default function BusinessPage({ prop1 }) {
 
     return (
-    <div><Dashboard/></div>
+        <>
+            <div><Dashboard/></div>
+            <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            >
+                <Button variant="contained" sx={{alignText: "center", marginTop: "15px"}}>Add New</Button>
+            </Box>
+        </>
     )
 }
