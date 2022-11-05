75b330bfe85638512c0d08297369a097006aed4d
barrowben <babarrow1@sheffield.ac.uk>
2022-11-05 16:00:54 +0000

    menu bar for customer page

--- src/customer/CustomerPage.jsx
+++ src/customer/CustomerPage.jsx
@@ -1,16 +1,22 @@
 import Map from "react-map-gl";
+import MenuBar from "./MenuBar";
 
 export default function CustomerPage() {
     return (
-        <Map
-            initialViewState={{
-                longitude: -1.477,
-                latitude: 53.378,
-                zoom: 13.21
-            }}
-            style={{width: '100vw', height: '100vh'}}
-            mapStyle="mapbox://styles/illuminatiboat/cla42292y00ns14p07a4ipgzp"
-            mapboxAccessToken="pk.eyJ1IjoiaWxsdW1pbmF0aWJvYXQiLCJhIjoiY2poMjh5eWRhMDFxODJxbXJuaXRvMmJtdCJ9.wiImXdAqSjsqZTaaG5VvXQ"
-        />
+        <div>
+            <Map
+                initialViewState={{
+                    longitude: -1.477,
+                    latitude: 53.378,
+                    zoom: 13.21
+                }}
+                style={{width: '100vw', height: '100vh'}}
+                mapStyle="mapbox://styles/illuminatiboat/cla42292y00ns14p07a4ipgzp"
+                mapboxAccessToken="pk.eyJ1IjoiaWxsdW1pbmF0aWJvYXQiLCJhIjoiY2poMjh5eWRhMDFxODJxbXJuaXRvMmJtdCJ9.wiImXdAqSjsqZTaaG5VvXQ"
+            >
+            <MenuBar/>
+            </Map>
+
+        </div>
     );
 }
