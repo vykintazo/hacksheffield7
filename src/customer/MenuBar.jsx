236c25e7cb1824338fbac294a06f52bf2fc40add
Jed-g <jedrek.golebiewski@gmail.com>
2022-11-06 09:01:21 +0000

    Fix drawer error

--- src/customer/MenuBar.jsx
+++ src/customer/MenuBar.jsx
@@ -1,20 +1,20 @@
-import {useEffect, useState} from 'react';
-import {Global} from '@emotion/react';
-import {styled} from '@mui/material/styles';
-import {grey} from '@mui/material/colors';
+import { useEffect, useState } from 'react';
+import { Global } from '@emotion/react';
+import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import {Button, IconButton, List, ListItemButton, ListItemText, Stack} from "@mui/material";
-import {ArrowBack} from "@mui/icons-material";
+import { Button, IconButton, List, ListItemButton, ListItemText, Stack } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
 
 const drawerBleeding = 56;
 
-const StyledBox = styled(Box)(({theme}) => ({
+const StyledBox = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
 }));
 
-const Puller = styled(Box)(({theme}) => ({
+const Puller = styled(Box)(({ theme }) => ({
     width: 30,
     height: 6,
     backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
@@ -24,7 +24,7 @@ const Puller = styled(Box)(({theme}) => ({
     left: 'calc(50% - 15px)'
 }));
 
-export default function MenuBar({businesses, offers, selectedBusiness, onSelectedBusinessChange}) {
+export default function MenuBar({ businesses, offers, selectedBusiness, onSelectedBusinessChange }) {
     const [open, setOpen] = useState(false);
 
     const toggleDrawer = (newOpen) => () => {
@@ -45,7 +45,6 @@ export default function MenuBar({businesses, offers, selectedBusiness, onSelecte
 
     return (
         <div>
-            <Button onClick={toggleDrawer(true)}>Open</Button>
             <Global
                 styles={{
                     '.MuiDrawer-root > .MuiPaper-root': {
@@ -54,7 +53,14 @@ export default function MenuBar({businesses, offers, selectedBusiness, onSelecte
                     },
                 }}
             />
-
+            <Box
+            onClick={toggleDrawer(true)}
+            sx={{
+                display: { xs: "none", md: open ? "none" : "flex" }, justifyContent: "center", position: "absolute",
+                bottom: "20px", zIndex: "100000", width: "100vw"
+            }}>
+                <Button onClick={toggleDrawer(true)}>Open</Button>
+            </Box>
             <SwipeableDrawer
                 anchor="bottom"
                 open={open}
@@ -78,8 +84,8 @@ export default function MenuBar({businesses, offers, selectedBusiness, onSelecte
                         left: 0
                     }}
                 >
-                    <Puller/>
-                    <Typography variant="h5" sx={{p: 2}}>Nearby Offers</Typography>
+                    <Puller sx={{ display: { md: "none" } }} />
+                    <Typography variant="h5" sx={{ p: 2 }}>Nearby Offers</Typography>
                 </StyledBox>
                 <StyledBox
                     sx={{
@@ -91,22 +97,22 @@ export default function MenuBar({businesses, offers, selectedBusiness, onSelecte
                 >
                     {!selectedBusiness ? (<List>
                         {businesses?.map((businessUser) => {
-                            const {business, uid} = businessUser;
+                            const { business, uid } = businessUser;
                             const businessOffers = offers?.filter((offer) => offer.uid === uid);
                             return (
                                 <ListItemButton key={uid} onClick={() => onSelectedBusinessChange(businessUser)}>
                                     <ListItemText
                                         primary={business.name}
-                                        secondary={`${businessOffers.length} offer(s) available.`}/>
+                                        secondary={`${businessOffers.length} offer(s) available.`} />
                                 </ListItemButton>
                             )
                         })}
                     </List>) : (
-                        <Box sx={{width: '100%'}}>
-                            <Stack direction="row" alignItems="center" sx={{width: '100%'}}>
-                                <IconButton onClick={() => onSelectedBusinessChange(null)}><ArrowBack/></IconButton>
-                                <Typography sx={{flexGrow: 1}}
-                                            variant="h6">{selectedBusiness.business.name}</Typography>
+                        <Box sx={{ width: '100%' }}>
+                            <Stack direction="row" alignItems="center" sx={{ width: '100%' }}>
+                                <IconButton onClick={() => onSelectedBusinessChange(null)}><ArrowBack /></IconButton>
+                                <Typography sx={{ flexGrow: 1 }}
+                                    variant="h6">{selectedBusiness.business.name}</Typography>
                             </Stack>
                             <List>
                                 {offers?.filter((offer) => offer.uid === selectedBusiness.uid)?.map((offer) => {
@@ -116,7 +122,7 @@ export default function MenuBar({businesses, offers, selectedBusiness, onSelecte
                                         <ListItemButton key={offer.id} onClick={() => null}>
                                             <ListItemText
                                                 primary={offer.offerName}
-                                                secondary={offer.description}/>
+                                                secondary={offer.description} />
                                         </ListItemButton>
                                     )
                                 })}
