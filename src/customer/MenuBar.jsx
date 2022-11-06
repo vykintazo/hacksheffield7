b26e45a6d84669a87c91bcf465632033757888e7
Vykintas Valuzis <vykintazo@gmail.com>
2022-11-06 06:14:59 +0000

    Marker interaction

--- src/customer/MenuBar.jsx
+++ src/customer/MenuBar.jsx
@@ -1,85 +1,126 @@
-import {React, useState} from 'react';
-import { Global } from '@emotion/react';
-import { styled } from '@mui/material/styles';
-import { grey } from '@mui/material/colors';
+import {useEffect, useState} from 'react';
+import {Global} from '@emotion/react';
+import {styled} from '@mui/material/styles';
+import {grey} from '@mui/material/colors';
 import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import {Button} from "@mui/material";
+import {Button, IconButton, List, ListItemButton, ListItemText} from "@mui/material";
+import {ArrowBack} from "@mui/icons-material";
 
 const drawerBleeding = 56;
 
-const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+const StyledBox = styled(Box)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
 }));
 
-const Puller = styled(Box)(({ theme }) => ({
-  width: 30,
-  height: 6,
-  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
-  borderRadius: 3,
-  position: 'absolute',
-  top: 8,
-  left: 'calc(50% - 15px)'
+const Puller = styled(Box)(({theme}) => ({
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+    borderRadius: 3,
+    position: 'absolute',
+    top: 8,
+    left: 'calc(50% - 15px)'
 }));
 
-export default function SwipeableEdgeDrawer() {
-  const [open, setOpen] = useState(false);
+export default function MenuBar({businesses, offers, selectedBusiness, onSelectedBusinessChange}) {
+    const [open, setOpen] = useState(false);
 
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
 
-  return (
-    <div>
-        <Button onClick= {toggleDrawer(true)}>Open</Button>
-      <Global
-        styles={{
-          '.MuiDrawer-root > .MuiPaper-root': {
-            height: `calc(50% - ${drawerBleeding}px)`,
-            overflow: 'visible',
-          },
-        }}
-      />
+    useEffect(() => {
+        if (!open && selectedBusiness) {
+            setOpen(true);
+        }
+    }, [selectedBusiness]);
 
-      <SwipeableDrawer
-        anchor="bottom"
-        open={open}
-        onClose={toggleDrawer(false)}
-        onOpen={toggleDrawer(true)}
-        swipeAreaWidth={drawerBleeding}
-        disableSwipeToOpen={false}
-        ModalProps={{
-          keepMounted: true,
-        }}
-      >
-        <StyledBox
-          sx={{
-            background: 'white',
-            position: 'absolute',
-            top: -drawerBleeding,
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-            visibility: 'visible',
-            right: 0,
-            left: 0
-          }}
-        >
-          <Puller/>
-          <Typography sx={{ p: 2, color: 'text.secondary' }}>Nearby Offers</Typography>
-        </StyledBox>
-        <StyledBox
-          sx={{
-            px: 2,
-            pb: 2,
-            height: '100%',
-            overflow: 'auto',
-          }}
-        >
-          <Skeleton variant="rectangular" height="100%" />
-        </StyledBox>
-      </SwipeableDrawer>
-      </div>
-  );
+    useEffect(() => {
+        if (!open) {
+            onSelectedBusinessChange(null);
+        }
+    }, [open]);
+
+    return (
+        <div>
+            <Button onClick={toggleDrawer(true)}>Open</Button>
+            <Global
+                styles={{
+                    '.MuiDrawer-root > .MuiPaper-root': {
+                        height: `calc(50% - ${drawerBleeding}px)`,
+                        overflow: 'visible',
+                    },
+                }}
+            />
+
+            <SwipeableDrawer
+                anchor="bottom"
+                open={open}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+                swipeAreaWidth={drawerBleeding}
+                disableSwipeToOpen={false}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+            >
+                <StyledBox
+                    sx={{
+                        background: 'white',
+                        position: 'absolute',
+                        top: -drawerBleeding,
+                        borderTopLeftRadius: 8,
+                        borderTopRightRadius: 8,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0
+                    }}
+                >
+                    <Puller/>
+                    <Typography variant="h5" sx={{p: 2}}>Nearby Offers</Typography>
+                </StyledBox>
+                <StyledBox
+                    sx={{
+                        px: 2,
+                        pb: 2,
+                        height: '100%',
+                        overflow: 'auto',
+                    }}
+                >
+                    {!selectedBusiness ? (<List>
+                        {businesses?.map((businessUser) => {
+                            const {business, uid} = businessUser;
+                            const businessOffers = offers?.filter((offer) => offer.uid === uid);
+                            return (
+                                <ListItemButton key={uid} onClick={() => onSelectedBusinessChange(businessUser)}>
+                                    <ListItemText
+                                        primary={business.name}
+                                        secondary={`${businessOffers.length} offer(s) available.`}/>
+                                </ListItemButton>
+                            )
+                        })}
+                    </List>) : (
+                        <Box sx={{width: '100%'}}>
+                            <Box sx={{width: '100%'}}>
+                                <IconButton onClick={() => onSelectedBusinessChange(null)}><ArrowBack/></IconButton>
+                            </Box>
+                            <List>
+                                {offers?.filter((offer) => offer.uid === selectedBusiness.uid)?.map((offer) => {
+                                    return (
+                                        <ListItemButton key={offer.id} onClick={() => null}>
+                                            <ListItemText
+                                                primary={offer.offerName}
+                                                secondary={offer.description}/>
+                                        </ListItemButton>
+                                    )
+                                })}
+                            </List>
+                        </Box>
+                    )}
+                </StyledBox>
+            </SwipeableDrawer>
+        </div>
+    );
 }
