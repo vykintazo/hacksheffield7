cd7e1c2f7884a310432b4ced7656b45413ffc290
Vykintas Valuzis <vykintazo@gmail.com>
2022-11-06 04:40:43 +0000

    Add geocode function

--- src/customer/CustomerPage.jsx
+++ src/customer/CustomerPage.jsx
@@ -1,26 +1,65 @@
-import Map, { GeolocateControl, Marker } from "react-map-gl";
+import Map, {GeolocateControl} from "react-map-gl";
 import MenuBar from "./MenuBar";
 import VenueMarker from "../components/VenueMarker";
-import { getAuth } from "firebase/auth";
-import { useFirebaseApp, useSigninCheck } from "reactfire";
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import {getAuth} from "firebase/auth";
+import {useFirebaseApp, useFirestore, useFirestoreCollectionData, useSigninCheck} from "reactfire";
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {collection, doc, getDoc, query, where} from "firebase/firestore";
+
 
 export default function CustomerPage() {
-
     const markers = [
-        { location: [53.38089284303829, -1.4766664972820758], label: "Molly Malone's Irish Tavern" },
-        { location: [53.379374277675964, -1.4702493564818155], label: "Costa Coffee" },
-        { location: [53.38020004206791, -1.4792187983038223], label: "KFC Sheffield" },
-        { location: [53.380888801054645, -1.479620700831369], label: "The Red Deer" },
+        {location: [53.38089284303829, -1.4766664972820758], label: "Molly Malone's Irish Tavern"},
+        {location: [53.379374277675964, -1.4702493564818155], label: "Costa Coffee"},
+        {location: [53.38020004206791, -1.4792187983038223], label: "KFC Sheffield"},
+        {location: [53.380888801054645, -1.479620700831369], label: "The Red Deer"},
     ]
+    const [businesses, setBusinesses] = useState([]);
 
-    const { data: signInCheckResult } = useSigninCheck();
+    const firestore = useFirestore();
+    const offerCollection = collection(firestore, 'offers');
+    const offerQuery = query(offerCollection, where('end', '<=', new Date()))
+
+    const {status, data: offers} = useFirestoreCollectionData(offerCollection, {
+        idField: 'id', // this field will be added to the object created from each document
+    });
+
+
+    const {data: signInCheckResult} = useSigninCheck();
 
     const authInstance = getAuth(useFirebaseApp());
 
     const navigateTo = useNavigate();
+
+
+    useEffect(() => {
+        const getBusinessUser = async (businessId) => {
+            if (businesses.some((b) => b.uid === businessId)) {
+                return null;
+            }
+            const businessSnap = await getDoc(doc(firestore, "users", businessId));
+            if (businessSnap.exists()) {
+                const businessUser = businessSnap.data();
+                console.log(businessUser);
+                return businessUser;
+            }
+            return null;
+        };
+        const groupOffers = async (offers) => {
+            await Promise.allSettled(offers.map(async (offer) => {
+                const businessId = offer.uid;
+                const businessUser = await getBusinessUser(businessId);
+                if (businessUser) {
+                    setBusinesses((prev) => [...prev, businessUser])
+                }
+            }));
+        };
+
+        groupOffers(offers);
+    }, [offers, firestore])
+
 
     useEffect(() => {
         signInCheckResult?.signedIn === false && navigateTo("/auth");
@@ -34,12 +73,13 @@ export default function CustomerPage() {
                     latitude: 53.378,
                     zoom: 13.21
                 }}
-                style={{ width: '100vw', height: '100vh' }}
+                style={{width: '100vw', height: '100vh'}}
                 mapStyle="mapbox://styles/illuminatiboat/cla42292y00ns14p07a4ipgzp"
                 mapboxAccessToken=import.meta.env.APP_MAPBOX_TOKEN
             >
-                <Button onClick={() => authInstance.signOut()} variant="contained" sx={{ backgroundColor: "red", position: "absolute", top: 20, right: 20 }}>Log Out</Button>
-                <MenuBar />
+                <Button onClick={() => authInstance.signOut()} variant="contained"
+                        sx={{backgroundColor: "red", position: "absolute", top: 20, right: 20}}>Log Out</Button>
+                <MenuBar/>
                 <GeolocateControl
                     trackUserLocation={true}
                     position="bottom-right"
@@ -47,15 +87,19 @@ export default function CustomerPage() {
                         marginBottom: 40
                     }}
                 />
-                {markers.map((marker, i) => (
-                    <VenueMarker
-                        key={i}
-                        lon={marker.location[1]}
-                        lat={marker.location[0]}
-                        label={marker.label}
-                        offerCount={5}
-                    />
-                ))}
+                {businesses.map((businessUser, i) => {
+                    const { business, uid } = businessUser
+
+                    return(
+                        <VenueMarker
+                            key={uid}
+                            lon={business.location.lon}
+                            lat={business.location.lat}
+                            label={business.name}
+                            offerCount={5}
+                        />
+                    );
+                })}
             </Map>
         </div>
     );
