520ffa093992e7c46e7269be23eecc4aa5bb59a0
Jed-g <jedrek.golebiewski@gmail.com>
2022-11-06 04:34:28 +0000

    Create customer page redirect

--- src/InitialForm.jsx
+++ src/InitialForm.jsx
@@ -55,6 +55,8 @@ export default () => {
         await updateDoc(doc(db, "users", signInCheckResult.user.uid), {
             userRole: "business"
         });
+
+        navigateTo("/form");
     }
 
     return <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw" }}>
